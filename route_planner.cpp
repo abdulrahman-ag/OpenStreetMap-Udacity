2034750a98b19fe9c83d04c2b6a25a9321f9375f
abdulrahman-ag <44980801+abdulrahman-ag@users.noreply.github.com>
2019-10-27 08:50:04 +0300

    fixed

--- route_planner.cpp
+++ route_planner.cpp
@@ -108,6 +108,7 @@ void RoutePlanner::AStarSearch() {
     
     // TODO: Implement your solution here.
     current_node = start_node;
+  	current_node->visited = true; 
     AddNeighbors(current_node);
     
     while(open_list.size() != 0){
